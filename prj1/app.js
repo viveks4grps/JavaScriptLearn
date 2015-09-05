7cc223c37c81f266878a19173b55f176daa8e6a4
vivek <viveks4grps@gmail.com>
2015-09-05 10:30:00 +0530

    More changes to Java Script from the training

--- prj1/app.js
+++ prj1/app.js
@@ -3,3 +3,32 @@ var a = 'Hello World!';
 function b() {
     console.log('Called b!');
 }
+
+//console.log(a);
+b();
+if(a === undefined) {
+    console.log("a is undefined");
+}
+else {
+    console.log("a is defined::"+a);
+}
+
+//Asynchronous Event Handling
+function waitThreeSeconds() {
+    var ms = 3000 + new Date().getTime();
+    while(new Date() < ms) {
+        //Do nothing
+    }
+    console.log('finished waiting');
+}
+
+function clickHandler() {
+    console.log('click event!');
+    console.log('In clickHandler I can see a::'+a);
+}
+
+//listen for the click event
+document.addEventListener('click',clickHandler);
+
+waitThreeSeconds();
+console.log('finished execution');
